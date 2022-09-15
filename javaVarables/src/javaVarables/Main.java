37bda880595756c161bc0a5fcead1eb754f93115
TReid96 <temarkusr@gmail.com>
2022-09-14 20:48:58 -0400

    DataTypes

---
+++ javaVarables/src/javaVarables/Main.java
@@ -0,0 +1,28 @@
+package javaVarables;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		System.out.println ("TReid");
+boolean b1 =true;
+byte bBeats = 100;
+short hotCold = -12000;
+long distance = -54367324567L;
+int far = 887345;
+double dots = 33.3;
+float air = 22.2f;
+char word = '\u8923';
+
+
+
+
+
+
+
+
+	
+	
+	
+	}
+}
